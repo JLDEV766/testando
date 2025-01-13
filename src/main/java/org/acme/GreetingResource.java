474589b540763da0cd54ee7dc1653eea9e0fce51
lucasmelo <lucas-melo59@hotmail.com>
2025-01-13 00:18:32 -0300

    first commit

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,16 +1,35 @@
 package org.acme;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Path("/hello")
 public class GreetingResource {
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from Quarkus REST";
+    @Produces(MediaType.APPLICATION_JSON)
+    public String teste(
+            @QueryParam("code") String code,
+            @QueryParam("state") String state
+    ) {
+        try {
+            // Criando um mapa para armazenar os parâmetros
+            Map<String, String> queryParams = new HashMap<>();
+            queryParams.put("code", code);
+            queryParams.put("state", state);
+
+            // Convertendo o mapa em JSON
+            return objectMapper.writeValueAsString(queryParams);
+
+        } catch (Exception e) {
+            return "{\"error\":\"Falha ao converter os parâmetros\"}";
+        }
     }
+
 }
